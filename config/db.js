3901a1ebf69ead64c292c5e3a2e08753fcdf8812
DubJ <jeremy.ah33@gmail.com>
2022-02-16 18:49:16 +0100

    set up project + bd co

---
+++ config/db.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://' + process.env.DB_USER_PASS + '@cluster0.utafe.mongodb.net/mernV2',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() =>console.log('Connexion à MongoDB réussie !'))
+  .catch(() =>console.log('Connexion à MongoDB échouée !'));
