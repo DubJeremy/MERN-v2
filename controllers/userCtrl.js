70ee6094bfbe51b54ae17e120b52490fb437791c
DubJ <jeremy.ah33@gmail.com>
2022-02-18 12:33:24 +0100

    get userInfo

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -47,3 +47,8 @@ exports.signin = (req, res, next) => {
   };
 
 // User ---------------------------------------
+exports.userInfo = (req, res, next) => {
+  User.findOne({ _id: req.params.id })
+    .then(user => res.status(200).json(( user )))
+    .catch(error => res.status(404).json({ error }));
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+
+const auth = require('../middlewares/auth');
 const userCtrl = require('../controllers/userCtrl');
 
 router.post('/signup', userCtrl.register);
 router.post('/signin', userCtrl.signin);
+
+router.get('/:id', userCtrl.userInfo);
+// router.put('/:id', auth, userCtrl.userUpdate);
+// router.delete('/:id', auth, userCtrl.userDelete);
+
 
 module.exports = router;
